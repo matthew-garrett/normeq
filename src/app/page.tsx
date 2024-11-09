867a29af01ee8bccdefef0f297c04e49f01cc123
matthew-garrett <matthew.garrett252@gmail.com>
2024-11-09 11:45:29 -0500

    api trouble shooting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,15 @@ const fetcher = (url: string): Promise<any[]> =>
   fetch(url).then((res) => res.json());
 
 export default function Home() {
-  const { data } = useSWR<any[]>("/api/zapier-webhook", fetcher);
+  // const { data } = useSWR<any[]>("/api/zapier-webhook", fetcher);
 
-  console.log({ data });
+  // console.log({ data });
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1>NORMEQ</h1>
-        <div>{JSON.stringify(data)}</div>
+        {/* <div>{JSON.stringify(data)}</div> */}
       </main>
     </div>
   );
