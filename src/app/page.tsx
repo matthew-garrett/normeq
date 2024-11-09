8c4bfc0f3cadf56883e84145d60ecaf55fc00300
matthew-garrett <matthew.garrett252@gmail.com>
2024-11-09 12:03:02 -0500

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
-import useSWR from "swr";
+// import useSWR from "swr";
 
-const fetcher = (url: string): Promise<any[]> =>
-  fetch(url).then((res) => res.json());
+// const fetcher = (url: string): Promise<any[]> =>
+//   fetch(url).then((res) => res.json());
 
 export default function Home() {
-  const { data } = useSWR<any[]>("/api/zapier-webhook", fetcher);
+  // const { data } = useSWR<any[]>("/api/zapier-webhook", fetcher);
 
-  console.log({ data });
+  // console.log({ data });
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
