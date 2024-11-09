069f9ef7ec6f865222b661c20315970be871af0e
matthew-garrett <matthew.garrett252@gmail.com>
2024-11-09 12:38:01 -0500

    adds prisma generate to build command

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 "use client";
-
 import { useEffect, useState } from "react";
-
-// import useSWR from "swr";
-
-// const fetcher = (url: string): Promise<any[]> =>
-//   fetch(url).then((res) => res.json());
 
 export default function Home() {
   const [data, setData] = useState(null);
